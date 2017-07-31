eb2ab8727224b82208a12efa90aa348acd13d0f0
etienne <squawkcpp@gmail.com>
2017-07-31 12:34:05 +0000

    add cmake files

---
+++ cmake/external/rapidxml_ns.cmake
@@ -0,0 +1,17 @@
+#https://github.com/svgpp/rapidxml_ns
+include(ExternalProject)
+
+ExternalProject_Add(
+  rapidxml_master
+  URL "https://github.com/svgpp/rapidxml_ns/archive/master.zip"
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  UPDATE_COMMAND ""
+  PATCH_COMMAND ""
+)
+ExternalProject_Get_Property(rapidxml_master source_dir)
+set(RAPIDXML_INCLUDE_DIR ${source_dir})
+set(RAPIDXML_LIBRARY rapidxml)
+add_library(${RAPIDXML_LIBRARY} INTERFACE IMPORTED)
+add_dependencies(${RAPIDXML_LIBRARY} rapidxml_master )
