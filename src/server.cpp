221f103bf1cb89e7a6c83b27c4b0050b2871b08a
etienne <squawkcpp@gmail.com>
2017-07-31 19:51:14 +0000

    make runnable

--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,7 @@
 
 #include "cxxopts.hpp"
 #include "fmt/format.h"
+#include "rapidxml_ns.hpp"
 #include "spdlog/spdlog.h"
 
 #include "http/mod/exec.h"
@@ -12,52 +13,6 @@
 #include "http/mod/method.h"
 
 #include "_utils.h"
-
-void signalHandler( int signum ) {
-    std::cout << "Interrupt signal (" << signum << ") received.\n";
-    exit( signum );
-}
-
-int main( int argc, char* argv[] ) {
-    //parse args
-    cxxopts::Options options("squawk upnp cli", "Command line interface for the squawk upnp media server.");
-    options.add_options()
-        ( "http-ip", "API Webserver IP-Adress to bind to.", cxxopts::value<std::string>(), "IP" )
-        ( "http-port", "API Webserver IP Port to bind to.", cxxopts::value<std::string>(), "PORT" )
-        ( "redis", "Redis Database (default: localhost)", cxxopts::value<std::string>()->default_value("localhost"), "HOST" )
-        ( "help", "Print help")
-      ;
-    options.parse(argc, argv);
-
-    if( options.count( "help" ) ) {
-         std::cout << options.help({"", "Group"}) << std::endl;
-         exit(0);
-    }
-
-    auto& _redis_server = options["redis"].as<std::string>();
-
-    std::string _ip = "localhost", _port = "8080";
-    if ( options.count( "http-ip" ) )
-    { _ip = options["http-ip"].as<std::string>(); }
-    if ( options.count( "http-port" ) )
-    { _port = options["http-port"].as<std::string>(); }
-
-    //start server
-    std::cout << "Start squawk upnp server." << std::endl;
-
-    /** Setup and start the HTTP Server **/
-    auto _web_server = std::shared_ptr< http::Server< http::HttpServer > >( new http::Server< http::HttpServer >( _ip, _port ) );
-    upnp::Server _server( _web_server, _redis_server, 6379 );
-
-    // register signal SIGINT and signal handler
-    signal(SIGINT, signalHandler);
-
-    while(1)
-    { sleep(1); }
-
-    //wait
-    return 0;
-}
 
 namespace upnp {
 
@@ -76,4 +31,14 @@ Server::Server(std::shared_ptr< http::Server< http::HttpServer > > web_server, /
         }),
         http::mod::Http() );
 }
+
+std::map< std::string, std::string > Server::parse( const std::string& body ) {
+    rapidxml_ns::xml_document<> doc;
+    doc.parse<0>( const_cast< char* >(  body.c_str() ) );
+    auto root_node = doc.first_node( "Envelope" );
+    for (rapidxml_ns::xml_node<> * __r_sieblings = root_node->first_node(); __r_sieblings; __r_sieblings = __r_sieblings->next_sibling() ) {
+        std::cout << "node: " << __r_siebling->name() << std::endl;
+    }
+}
+
 }//namespace upnp
--- src/server.h
+++ src/server.h
@@ -1,7 +1,9 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <map>
 #include <memory>
+#include <string>
 
 #include "http/server.h"
 #include "http/httpserver.h"
@@ -16,6 +18,11 @@ public:
     Server(std::shared_ptr< http::Server< http::HttpServer > > web_server, /** @param web_server the server to attach the API uri's. */
            const std::string& redis, /** @param redis redis host */
            const short port /** @param port the redis port. */ );
+
+    static std::map< std::string, std::string > parse( const std::string& body );
+
+private:
 };
+
 }//namespace upnp
 #endif // SERVER_H
